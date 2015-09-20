7c01849d76f4590f3c559203dd582a45ffda01da
nicholasruggeri <ruggerinicholas@gmail.com>
2015-09-20 13:04:58 +0200

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,46 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var prefix = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
 
-
-gulp.task('hello', function() {
-    console.log('Hello Terminal');
+gulp.task('styles', function() {
+  gulp.src('_assets/scss/*.scss')
+    .pipe(sass())
+    .pipe(prefix())
+    .pipe(cssmin())
+    .pipe(gulp.dest('web/css'))
+    .pipe(browserSync.stream());
 });
 
 
-gulp.task('default', ['hello']);
+gulp.task('scripts', function() {
+  gulp.src('_assets/js/**/*.js')
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('web/js'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('_assets/scss/**/*.scss', ['styles']);
+    gulp.watch('_assets/js/**/*.js', ['scripts']);
+});
+
+
+gulp.task('serve', ['styles'], function() {
+
+    browserSync.init({
+        server: "./web"
+    });
+
+    gulp.watch('_assets/scss/**/*.scss', ['styles']);
+    gulp.watch('_assets/js/**/*.js', ['scripts']);
+    gulp.watch("web/*.html").on('change', browserSync.reload);
+});
+
+
+gulp.task('default', ['serve']);
 
