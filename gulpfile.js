9ef5e130fe6b9e219bbd98ad4db6a138bf125365
nicholasruggeri <ruggerinicholas@gmail.com>
2015-09-30 23:17:18 +0200

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cssmin = require('gulp-cssmin');
-var prefix = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var cssmin      = require('gulp-cssmin');
+var prefix      = require('gulp-autoprefixer');
+var concat      = require('gulp-concat');
+var uglify      = require('gulp-uglify');
 var browserSync = require('browser-sync').create();
-var plumber = require('gulp-plumber');
-var notify = require("gulp-notify");
+var plumber     = require('gulp-plumber');
+var notify      = require("gulp-notify");
 
 
 gulp.task('styles', function() {
