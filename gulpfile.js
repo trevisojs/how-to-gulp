6f4de3f05d4f7cc738efcd65c76e9eb96a734cf3
nicholasruggeri <ruggerinicholas@gmail.com>
2015-10-01 21:01:30 +0200

    add comments in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,94 @@
+/*==========================================================
+How to Gulp
+Author: @trevisojs       - https://github.com/trevisojs
+Author: @ruggerinicholas - http://ruggeri.io
+==========================================================*/
+
+
+/**
+* List gulp dependencies
+**/
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
 var cssmin      = require('gulp-cssmin');
 var prefix      = require('gulp-autoprefixer');
 var concat      = require('gulp-concat');
 var uglify      = require('gulp-uglify');
-var browserSync = require('browser-sync').create();
 var plumber     = require('gulp-plumber');
 var notify      = require("gulp-notify");
+var browserSync = require('browser-sync').create();
 
 
+/**
+*
+* Styles
+* - Catch errors (gulp-plumber)
+* - Compile
+* - Autoprefixer
+* - Minify
+*
+**/
 gulp.task('styles', function() {
   gulp.src('_assets/scss/*.scss')
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(sass())
     .pipe(prefix())
     .pipe(cssmin())
-    .pipe(plumber.stop())
     .pipe(gulp.dest('web/css'))
     .pipe(browserSync.stream());
 });
 
 
+/**
+*
+* Scripts
+* - Catch errors (gulp-plumber)
+* - Concat
+* - Uglify
+*
+**/
 gulp.task('scripts', function() {
   gulp.src('_assets/js/**/*.js')
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(concat('scripts.js'))
     .pipe(uglify())
-    .pipe(plumber.stop())
     .pipe(gulp.dest('web/js'))
     .pipe(browserSync.stream());
 });
 
 
+/**
+*
+* Watch assets
+* - styles
+* - scripts
+*
+**/
 gulp.task('watch', function() {
     gulp.watch('_assets/scss/**/*.scss', ['styles']);
     gulp.watch('_assets/js/**/*.js', ['scripts']);
 });
 
 
+/**
+*
+* Build assets
+* - styles
+* - scripts
+*
+**/
 gulp.task('build', ['styles', 'scripts'], function() {
     console.log('build assets')
 });
 
 
+/**
+*
+* Serve - BrowserSync.io
+* - Watch CSS, JS & HTML for changes
+* - View project at: localhost:3000
+*
+**/
 gulp.task('serve', ['build'], function() {
     browserSync.init({
         server: "./web"
@@ -53,4 +99,10 @@ gulp.task('serve', ['build'], function() {
 });
 
 
+/**
+*
+* Default task
+* - launch serve
+*
+**/
 gulp.task('default', ['serve']);
